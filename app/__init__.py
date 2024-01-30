2cb56fd751774a4eef106dc400a8d0b76057f7cc
Davon Wilson <davontech3@gmail.com>
2024-01-29 22:01:12 -0500

    setup server for react

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,11 @@ import os
 from .authentication.routes import auth
 from models import db , login_manager
 from .api.routes import site
+from flask_cors import CORS
 
 
 app = Flask(__name__, template_folder='./site/site_templates')
+CORS(app)
 
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'config.py')
 app.config.from_pyfile(config_path)
@@ -15,6 +17,7 @@ login_manager.init_app(app)
 db.init_app(app)
 app.register_blueprint(auth)
 app.register_blueprint(site)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
